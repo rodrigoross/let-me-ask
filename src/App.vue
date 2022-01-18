3eafab45611bc2d3bad0924eb2406cdba780a8e7
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-01-18 15:51:36 -0400

    chore: preparado estilo base e layout inicial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --white: #fff;
+
+  --gray-50: #f8f8f8;
+  --gray-100: #e2e2e2;
+  --gray-200: #a8a8b3;
+  --gray-500: #737380;
+  --gray-800: #29292e;
+
+  // --green-500: #04d361;
+  --red-500: #ea4335;
+  --red-700: #e73f5d;
+
+  --pink: #e559f9;
+
+  // --purple-300: #9f75ff;
+
+  --blue-100: #f4f8ff;
+  --blue-800: #835afd;
+}
+
+body {
+  background: var(--gray-50);
+  color: var(--gray-800);
+}
+
+body,
+input,
+button,
+textarea {
+  font: 400 16px "Nunito", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
